6f0f953e6f658db24e8a8405db52e059cfebc58a
luxiaotao1123 <t1341870251@63.com>
2023-02-28 17:22:13 +0800

    #

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { createApp } from 'vue'
-
 import App from './App.vue'
-
-import { createPinia } from 'pinia' 
-
+import store from './store/index'
+import { piniaPlugin } from './store/index'
 import './style.css'
 import './assets/css/reset.less'
 
-const store = createPinia();
 let app = createApp(App);
 
-app.use(store);
+store.use(piniaPlugin({
+    key: 'pinia'
+})); // 装载pinia缓存插件
 
+app.use(store);
 app.mount('#app')
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,35 @@
-import { defineStore } from 'pinia';
+import { createPinia, PiniaPluginContext, defineStore } from 'pinia';
+import { toRaw } from 'vue';
 import { Names } from './store-name'
 
+// 注册pinia实例
+export default createPinia();
+
+// pinia状态缓存
+const __piniaKey__ = "cool";
+type Options = {
+    key?:string
+}
+const setStorage = (key:string, val:any) => {
+    localStorage.setItem(key, JSON.stringify(val));
+}
+const getStorage = (key:string) => {
+    return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key) as string) : {}
+}
+export const piniaPlugin = (options:Options) => {
+    return (context: PiniaPluginContext) => {
+        const { store } = context;
+        const data = getStorage(`${options?.key ?? __piniaKey__} - ${store.$id}`)
+        store.$subscribe(()=> {
+            setStorage(`${options?.key ?? __piniaKey__} - ${store.$id}`, toRaw(store.$state));
+        })
+        return {
+            ...data
+        }
+    }
+}
+
+// store -------
 export const useTestStore = defineStore(Names.TEST, {
     state: () => {
         return {
