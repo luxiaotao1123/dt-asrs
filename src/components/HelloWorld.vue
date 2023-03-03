8ed3d6fa602b7a57c78aa69011577755f2165cae
luxiaotao1123 <t1341870251@63.com>
2023-03-03 14:18:50 +0800

    #

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,16 +2,16 @@
   <div id="container"></div>
 </template>
 
-<script lang='ts' setup>
+<script setup>
 import { ref, reactive, onMounted } from 'vue'
 import Base3D from '../utils/Base3D'
-// import Player from '../utils/Player'
+import Player from '../utils/Player'
 
 onMounted(() => {
-  const base3D = new Base3D('#container');
-  base3D.startup();
-  // const player = new Player('#container');
-  // player.startup();
+  // const base3D = new Base3D('#container');
+  // base3D.startup();
+  const player = new Player('#container');
+  player.startup();
 })
 </script>
 
